b19776d64eb9792b356ebcde9eed86f2651606fc
Reno Provine <rprovine@gmail.com>
2013-11-27 17:19:57 -0600

    added table of contents exercise

---
+++ table_of_contents.rb
@@ -0,0 +1,10 @@
+title = "Table of Contents".center(50)
+chapter_1 = "Chapter 1:  Getting Started".ljust(25) + "page 1".ljust(25)
+chapter_2 = "Chapter 2:  Numbers".ljust(25) + "page 9".ljust(25)
+chapter_3 = "Chapter 3:  Letters".ljust(25) + "page 13".ljust(25)
+
+puts title
+puts
+puts chapter_1
+puts chapter_2
+puts chapter_3
