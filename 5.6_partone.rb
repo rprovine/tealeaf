99b1bf16d999805f3be8fa11ab43a7be8f3d6ce6
Reno Provine <rprovine@gmail.com>
2013-11-27 12:03:00 -0600

    adding new

---
+++ 5.6_partone.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, what is your first name?'
+first_name = gets.chomp
+
+puts 'May I know have your middle name?'
+middle_name = gets.chomp
+
+puts 'May I finally have your last name?'
+last_name = gets.chomp
+
+puts 'Your full name is ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
+
---
+++ 5.6_parttwo.rb
@@ -0,0 +1,6 @@
+puts 'What is your favorite number?'
+number = gets.chomp
+
+puts 'A bigger and better favorite number would be' + number + 1.to_s
+
+
